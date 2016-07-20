513b71408e429998f7bcff4d7bcfd40d75115054
danci7 <askyhost@gmail.com>
2016-07-20 12:27:59 +0200

    Revert "Update email_settings.php"
    This reverts commit 843e55e45744358fd2ca500700f7f0d391ffdfa4.

--- email_settings.php
+++ email_settings.php
@@ -1,7 +1,7 @@
 <?php
 
 // FIXME: Update this to your desired email address.
-$recipient = "askyhost@gmail.com";
+$recipient = "your@email.com";
 
 // FIXME: Update subject if necessary.
 $subject = "New contact from $name";
