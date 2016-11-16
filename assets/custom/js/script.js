c22d45b18bdf9d469618dd6a21e3cba941166872
nebojsa <nljub@yahoo.com>
2016-11-16 21:07:41 +0100

    loeschen von nicht benutzten bildern

--- assets/custom/js/script.js
+++ assets/custom/js/script.js
@@ -10,35 +10,5 @@
 
     });
 
-    $(document).ready(function () {
-
-
-        /* Sticky Scroll */
-
-        $(".sticky-column").stickit({
-            screenMinWidth: 1024    // apply if width >= 1024
-        });
-
-        /* Back to top */
-        (function () {
-            $("#back-top").hide();
-
-            $(window).scroll(function () {
-                if ($(this).scrollTop() > 100) {
-                    $('#back-top').fadeIn();
-                } else {
-                    $('#back-top').fadeOut();
-                }
-            });
-
-            $('#back-top a').click(function () {
-                $('body,html').animate({
-                    scrollTop: 0
-                }, 600);
-                return false;
-            });
-        })();
-
-    });
 
 })(jQuery);
